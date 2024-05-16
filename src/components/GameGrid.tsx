597103cb595658cd6a5703ff2e0ac53edb82d5c3
chenment <chenment@gmail.com>
2024-05-16 22:24:38 +0800

    Implement infinite queries

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,45 @@
-import { SimpleGrid } from "@chakra-ui/react"
+import { Box, Button, SimpleGrid } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
 import { GameQuery } from "../App"
+import React from "react"
 
 interface Props {
   gameQuery: GameQuery
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery)
+  const { data, error, isLoading, hasNextPage, isFetchingNextPage, fetchNextPage } = useGames(gameQuery)
   const skeletons = Array.from({ length: 6 })
 
   if (error) return <p>{error.message}</p>
 
   return (
-    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" spacing={6}>
-      {isLoading && skeletons.map((_, i) => (
-        <GameCardContainer key={i}>
-          <GameCardSkeleton />
-        </GameCardContainer>
-      ))}
-      {data?.results.map((game) => (
-        <GameCardContainer key={game.id}>
-          <GameCard game={game} />
-        </GameCardContainer>
-      ))}
-    </SimpleGrid>
+    <Box padding="10px">
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+        {isLoading && skeletons.map((_, i) => (
+          <GameCardContainer key={i}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+        {data?.pages.map((page, index) =>
+          <React.Fragment key={index}>
+            {page.results.map(game =>
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            )}
+          </React.Fragment>
+        )}
+      </SimpleGrid>
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()} marginY={5}>
+          {isFetchingNextPage ? 'Loading...' : 'Load More'}
+        </Button>
+      )}
+    </Box>
   )
 }
 
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -8,13 +8,13 @@ export interface Genre {
   image_background: string;
 }
 
-const apiClient = new APIClient('/genres')
+const apiClient = new APIClient<Genre>('/genres')
 
 const useGenres = () => useQuery({
   queryKey: ['genres'],
   queryFn: apiClient.getAll,
   staleTime: 24 * 60 * 60 * 1000, // 24h
-  initialData: { count: genres.length, results: genres }
+  initialData: { count: genres.length, results: genres },
 })
 
 export default useGenres
