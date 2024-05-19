95fe6a0641fa4c520883925e1933df4905650d2b
chenment <chenment@gmail.com>
2024-05-19 17:04:56 +0800

    Build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query"
+import APIClient from "../services/api-client"
+import { screenshot } from "../entities/Screenshot"
+
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<screenshot>(`/games/${gameId}/screenshots`)
+
+  return useQuery({
+    queryKey: ['screenshots', gameId],
+    queryFn: apiClient.getAll
+  })
+}
+
+export default useScreenshots
